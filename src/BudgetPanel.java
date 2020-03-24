1d42fa433aab3651e58c6da85e547252b4d05615
Brendan Perry <brendanperry05@gmail.com>
2020-03-23 21:47:46 -0400

    Adding table

--- src/BudgetPanel.java
+++ src/BudgetPanel.java
@@ -7,6 +7,7 @@ import java.awt.Color;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 
 @SuppressWarnings("serial")
 public class BudgetPanel extends JPanel {
@@ -14,20 +15,35 @@ public class BudgetPanel extends JPanel {
 	public BudgetPanel() {
 		setLayout(new BorderLayout(0, 0));
 		
+		// EXPENSES PANEL
+		
 		JPanel expensesPanel = new JPanel();
+		expensesPanel.setLayout(new BorderLayout(0, 0));
 		JLabel expensesText = new JLabel("Expenses");
-		expensesPanel.add(expensesText);
+		expensesPanel.add(expensesText, BorderLayout.NORTH);
 		expensesPanel.setBackground(Color.RED);
 		expensesPanel.setBorder(BorderFactory.createLineBorder(Color.black));
 		
+		String[] columnNames = {"Item", "Cost", "Day", "Repeating", "Notes", "Category"};
+		
+		String[][] data = {{"Toast", "$5.99", "15", "No", "", "Food"}, {"Tea", "$1.99", "15", "No", "", "Food"}};
+		
+		JTable expenses = new JTable(data, columnNames);
+		
+		expensesPanel.add(expenses, BorderLayout.CENTER);
+		
 		JPanel rightPanel = new JPanel();
 		rightPanel.setLayout(new BorderLayout(0, 0));
+		
+		// INCOME PANEL
 		
 		JPanel incomePanel = new JPanel();
 		JLabel incomeText = new JLabel("Income");
 		incomePanel.add(incomeText);
 		incomePanel.setBackground(Color.YELLOW);
 		incomePanel.setBorder(BorderFactory.createLineBorder(Color.black));
+		
+		// REVIEW PANEL
 		
 		JPanel reviewPanel = new JPanel();
 		JLabel reviewText = new JLabel("review");
